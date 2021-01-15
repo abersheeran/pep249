b629cb30ab892012a1938692af60fe6f0a1f77ac
Aber <me@abersheeran.com>
2021-01-15 11:03:06 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,17 +7,17 @@ with open("./README.md", "r", encoding="utf8") as file:
     long_description = file.read()
 
 
-setup_kwargs = {
-    "name": "pep249",
-    "version": "1.0.0",
-    "description": "Provide connection pool of PEP249.",
-    "long_description": long_description,
-    "long_description_content_type": "text/markdown",
-    "author": "abersheeran",
-    "author_email": "me@abersheeran.com",
-    "url": "https://github.com/abersheeran/pep249",
-    "license": "MIT",
-    "classifiers": [
+setup(
+    name="pep249",
+    version="1.0.0",
+    url="https://github.com/abersheeran/pep249",
+    description="Provide minimum implementation check and connection pool of PEP249.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="abersheeran",
+    author_email="me@abersheeran.com",
+    license="MIT",
+    classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         "License :: OSI Approved :: MIT License",
@@ -25,7 +25,6 @@ setup_kwargs = {
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
     ],
-    "py_modules": ["pep249"],
-}
-
-setup(**setup_kwargs)
+    python_requires=">=3.7.0",
+    py_modules=["pep249"],
+)
